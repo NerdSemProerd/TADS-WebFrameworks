9d4ed4fb9fda867b5a6cc23009c312a12a86aa29
NerdSemProerd <emanuellrossetto@hotmail.com>
2024-05-11 18:41:39 -0300

    Trabalho_clickes_V0.2

--- Trabalho_contador_clicks/trabalho.js
+++ Trabalho_contador_clicks/trabalho.js
@@ -1,29 +1,54 @@
-let pontosEquipe1 = 0;
-let pontosEquipe2 = 0;
-let usadoTruco = true; // Inicializado como true
+var pontosEquipe1 = 0
+var pontosEquipe2 = 0
+var usadoTruco = false
+var pontostruco = null
 
-function adicionarPonto(equipe) {
-    if (equipe === 1) {
-        pontosEquipe1++;
+function adicionarPonto1() {  
+    if(usadoTruco == true){
+        pontosEquipe1 = pontosEquipe1 +2
+    }
+    else{
+        pontosEquipe1++
+    }
+    document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
+}
+function adicionarPonto2() {  
+    if(usadoTruco == true){
+        pontosEquipe2 = pontosEquipe2 +2
+    }
+    else{
+        pontosEquipe2++
+    }
+    document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+}
+
+function removerPonto1() {
+    if(pontosEquipe1 > 0){
+        pontosEquipe1--
         document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
-    } else {
-        pontosEquipe2++;
-        document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
     }
 }
 
-function removerPonto(equipe) {
-    if (equipe === 1) {
-        pontosEquipe1 = Math.max(0, pontosEquipe1 - 1);
-        document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
-    } else {
-        pontosEquipe2 = Math.max(0, pontosEquipe2 - 1);
-        document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
-    }
+function removerPonto2() {
+    if(pontosEquipe2 > 0){
+    pontosEquipe2--
+    document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+}
 }
 
 function usarTruco() {
-    usadoTruco == true
-    document.getElementById('truco').classList.add('truco-ativo');
+    if(usadoTruco == false){
+        usadoTruco = true
+        document.getElementById('truco').classList.add('truco-ativo');
+    }
+    else{
+        usadoTruco = false
+        document.getElementById('truco').classList.remove('truco-ativo');
+    }
+    
+        
+    
+    
+    
     
 }
