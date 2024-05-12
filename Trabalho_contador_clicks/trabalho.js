d1c46f701b6a39d84d975ad1b4f149684713e12a
NerdSemProerd <emanuellrossetto@hotmail.com>
2024-05-12 17:54:21 -0300

    Trabalho_clicks-terminado

--- Trabalho_contador_clicks/trabalho.js
+++ Trabalho_contador_clicks/trabalho.js
@@ -2,22 +2,32 @@ var pontosEquipe1 = 0
 var pontosEquipe2 = 0
 var usadoTruco = false
 var pontostruco = null
+var vitoriasEquipe1 = 0
+var vitoriasEquipe2 = 0
+var placarEquipe1 = document.getElementById("Equipe1")
+var placarEquipe2 = document.getElementById("Equipe2")
 
 function adicionarPonto1() {  
     if(usadoTruco == true){
-        pontosEquipe1 = pontosEquipe1 +2
+        pontosEquipe1 = pontosEquipe1 +3
+        desligatruco()
+        verificapontos()
     }
     else{
         pontosEquipe1++
+        verificapontos()
     }
     document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
 }
 function adicionarPonto2() {  
     if(usadoTruco == true){
-        pontosEquipe2 = pontosEquipe2 +2
+        pontosEquipe2 = pontosEquipe2 +3
+        desligatruco()
+        verificapontos()
     }
     else{
         pontosEquipe2++
+        verificapontos()
     }
     document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
 }
@@ -35,7 +45,6 @@ function removerPonto2() {
     document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
 }
 }
-
 function usarTruco() {
     if(usadoTruco == false){
         usadoTruco = true
@@ -44,11 +53,43 @@ function usarTruco() {
     else{
         usadoTruco = false
         document.getElementById('truco').classList.remove('truco-ativo');
-    }
-    
-        
-    
-    
-    
-    
+    }  
 }
+function desligatruco() {
+    usadoTruco = false
+    document.getElementById('truco').classList.remove('truco-ativo');
+}
+function verificapontos(){
+    if (pontosEquipe1 >= 12){
+        vitoriasEquipe1++
+        Equipe1.innerHTML = "Vitórias da Equipe 1: " + vitoriasEquipe1;
+        zerarpontos()
+
+    }
+    if (pontosEquipe2 >= 12){
+        vitoriasEquipe2++
+        Equipe2.innerHTML = "Vitórias da Equipe 2: " + vitoriasEquipe2;
+        zerarpontos()
+    }
+}
+function zerarpontos(){
+    pontosEquipe1 = 0
+    pontosEquipe2 = 0
+    atualizar()
+}
+function reiniciar(){
+    pontosEquipe1 = 0
+    pontosEquipe2 = 0
+    vitoriasEquipe1 = 0
+    vitoriasEquipe2 = 0
+    atualizar()
+    desligatruco()
+}
+function atualizar(){
+    Equipe1.innerHTML = "Vitórias da Equipe 1: " + vitoriasEquipe1;
+    Equipe2.innerHTML = "Vitórias da Equipe 2: " + vitoriasEquipe2;
+    document.getElementById('pontosEquipe1').textContent = pontosEquipe1;
+    document.getElementById('pontosEquipe2').textContent = pontosEquipe2;
+}
+
+//document.getElementById("vencedor").innerHTML =
